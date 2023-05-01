eb897f7ef22ce88696de40875561593329a18b93
Aadarsha Acharya <connectwithadarsha@gmail.com>
2023-05-01 18:52:30 +0545

    fix: date format for test

--- src/lib/formatDateTime/index.ts
+++ src/lib/formatDateTime/index.ts
@@ -1,7 +1,7 @@
 import { DATE_TIME_FORMAT } from "@/consts";
 import { format, parseISO, isValid } from "date-fns";
 
-const ISO_STRING_REGEX = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$/;
+const ISO_STRING_REGEX = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}$/;
 
 export const formatDate = (date: string) => {
   const isIsoString = ISO_STRING_REGEX.test(date);
